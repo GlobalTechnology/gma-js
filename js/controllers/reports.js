125c398058447ef33968dfebf0022c1083445565
Brian Zoetewey <brian.zoetewey@ccci.org>
2015-03-06 15:47:16 +0200

    Reports WIP

---
+++ js/controllers/reports.js
@@ -0,0 +1,7 @@
+define( ['app', 'underscore', 'measurementService', 'goog!visualization,1,packages:[corechart]'], function ( app, _ ) {
+	app.controller( 'ReportsController', [
+		'$scope', '$document', 'measurementService', 'settings',
+		function ($scope, $document, measurementService, settings) {
+
+	}] );
+} );
