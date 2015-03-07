2ccce17a1313a67c8cceb4196c482af949825c2b
Brian Zoetewey <brian.zoetewey@ccci.org>
2015-03-07 10:01:59 +0200

    Reports WIP

--- js/controllers/reports.js
+++ js/controllers/reports.js
@@ -1,7 +1,33 @@
 define( ['app', 'underscore', 'measurementService', 'goog!visualization,1,packages:[corechart]'], function ( app, _ ) {
 	app.controller( 'ReportsController', [
 		'$scope', '$document', 'measurementService', 'settings',
-		function ($scope, $document, measurementService, settings) {
+		function ( $scope, $document, measurementService, settings ) {
 
-	}] );
+			// Debounced method to fetch Measurements at most once every 100 milliseconds
+			var getMeasurements = _.debounce( function () {
+				if ( typeof $scope.current.assignment !== 'undefined' && typeof $scope.current.period !== 'undefined' && typeof $scope.current.mcc !== 'undefined' ) {
+					$scope.current.isLoaded = false;
+					$scope.measurements = measurementService.getMeasurements( {
+						ministry_id: $scope.current.assignment.ministry_id,
+						mcc:         $scope.current.mcc,
+						historical:  true
+					}, function () {
+						$scope.current.isLoaded = true;
+					} );
+				}
+			}, 100 );
+
+			$scope.$watch( 'current.assignment.ministry_id', function () {
+				getMeasurements();
+			} );
+
+			$scope.$watch( 'current.mcc', function () {
+				getMeasurements();
+			} );
+
+			$scope.$watch( 'current.period', function () {
+				getMeasurements();
+			} );
+
+		}] );
 } );
