2f6747460e4498ca2043f6849446b3b64a141724
Brian Zoetewey <brian.zoetewey@ccci.org>
2015-04-16 12:01:13 -0400

    [#MMJS-46] scroll join ministry modal to top

--- src/js/controllers/join-ministry-ctrl.js
+++ src/js/controllers/join-ministry-ctrl.js
@@ -1,9 +1,13 @@
-(function () {
+(function ( $ ) {
 	'use strict';
 
 	function JoinMinistryCtrl( $scope, $modalInstance, ministries, allowClose ) {
 		$scope.ministries = ministries;
 		$scope.allowClose = allowClose;
+
+		window.setTimeout( function () {
+			window.parent.scrollTo( 0, 0 );
+		}, 10 );
 
 		$scope.join = function () {
 			$modalInstance.close( $scope.ministry );
@@ -15,4 +19,4 @@
 	}
 
 	angular.module( 'gma.controllers' ).controller( 'JoinMinistryCtrl', JoinMinistryCtrl );
-})();
+})( jQuery );
