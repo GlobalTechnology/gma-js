bfd88cb14a07680249c9ab2578a514cfb0d298d7
austinankur <austin.ankur@ithands.net>
2015-07-20 12:28:53 +0530

    [#MMJS-66] Rollback user preferences service functions for reuseablity

--- src/js/controllers/preference/user-preference-ctrl.js
+++ src/js/controllers/preference/user-preference-ctrl.js
@@ -9,9 +9,9 @@
             hide_reports_tab : "1"
         };
 
-        $scope.ministries = getFlatMinistry($rootScope.current.assignments);
+        $scope.ministries = UserPreference.getFlatMinistry($rootScope.current.assignments);
         $scope.options = $rootScope.current.user_preferences;
-        $scope.mccs = getMappedMCCS($rootScope.current.assignment.mccs,modelData.mccLabels);
+        $scope.mccs = UserPreference.getMappedMCCS($rootScope.current.assignment.mccs,modelData.mccLabels);
 
         $scope.savePreference = function (options) {
             UserPreference.savePreference(options)
@@ -24,14 +24,14 @@
         };
 
         $scope.changeMCCS = function(ministry_id){
-            if(ministry_id===""||ministry_id==null) {
+            if(ministry_id==="" || ministry_id === null ) {
                 $scope.mccs = [];
                 return false;
             }
             var ministry = _.find($scope.ministries,function(mini){
                 return (mini.ministry_id === ministry_id);
             });
-            $scope.mccs = getMappedMCCS(ministry.mccs,modelData.mccLabels);
+            $scope.mccs = UserPreference.getMappedMCCS(ministry.mccs,modelData.mccLabels);
 
         };
         $scope.changeMCCS($scope.options.preferred_ministry);
@@ -43,31 +43,6 @@
         $scope.cancel = function () {
             $modalInstance.dismiss('cancel');
         };
-
-        function getMappedMCCS(mccs, mccLabels){
-            var mapped_mccs = [];
-            _.each(mccs, function (mcc) {
-                var customObject = {};
-                customObject.mcc = mcc;
-                customObject.mccLabel = mccLabels[mcc];
-                mapped_mccs.push(customObject);
-            });
-            return mapped_mccs;
-        }
-
-        function getFlatMinistry(assignments){
-            var ministries = [];
-            angular.forEach(assignments, flattenMinistry);
-
-            function flattenMinistry(ministry) {
-                ministries.push(ministry);
-                if (ministry.hasOwnProperty('sub_ministries') && _.size(ministry.sub_ministries) > 0) {
-                    angular.forEach(ministry.sub_ministries, flattenMinistry);
-                }
-            }
-
-            return ministries;
-        }
 
     }
 
