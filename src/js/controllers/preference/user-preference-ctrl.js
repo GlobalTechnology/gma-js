50c424912a787e204e768d4531e105431cd2fc00
austinankur <austin.ankur@ithands.net>
2015-07-20 10:10:18 +0530

    [#MMJS-66] Updated user preference module

--- src/js/controllers/preference/user-preference-ctrl.js
+++ src/js/controllers/preference/user-preference-ctrl.js
@@ -2,9 +2,16 @@
     'use strict';
     function UserPreferenceCtrl($scope, $rootScope, $modalInstance, modelData, UserPreference) {
         $scope.options = {};
-        $scope.ministries = UserPreference.getFlatMinistry($rootScope.current.assignments);
+
+        $scope.options = {
+            preferred_ministry : $rootScope.current.user_preferences.preferred_ministry || $rootScope.current.assignment.ministry_id,
+            preferred_mcc : "",
+            hide_reports_tab : "1"
+        };
+
+        $scope.ministries = getFlatMinistry($rootScope.current.assignments);
         $scope.options = $rootScope.current.user_preferences;
-        $scope.mccs = UserPreference.getMappedMCCS($rootScope.current.assignment.mccs,modelData.mccLabels);
+        $scope.mccs = getMappedMCCS($rootScope.current.assignment.mccs,modelData.mccLabels);
 
         $scope.savePreference = function (options) {
             UserPreference.savePreference(options)
@@ -16,6 +23,19 @@
 
         };
 
+        $scope.changeMCCS = function(ministry_id){
+            if(ministry_id===""||ministry_id==null) {
+                $scope.mccs = [];
+                return false;
+            }
+            var ministry = _.find($scope.ministries,function(mini){
+                return (mini.ministry_id === ministry_id);
+            });
+            $scope.mccs = getMappedMCCS(ministry.mccs,modelData.mccLabels);
+
+        };
+        $scope.changeMCCS($scope.options.preferred_ministry);
+
         $scope.close = function () {
             $modalInstance.dismiss();
         };
@@ -23,6 +43,31 @@
         $scope.cancel = function () {
             $modalInstance.dismiss('cancel');
         };
+
+        function getMappedMCCS(mccs, mccLabels){
+            var mapped_mccs = [];
+            _.each(mccs, function (mcc) {
+                var customObject = {};
+                customObject.mcc = mcc;
+                customObject.mccLabel = mccLabels[mcc];
+                mapped_mccs.push(customObject);
+            });
+            return mapped_mccs;
+        }
+
+        function getFlatMinistry(assignments){
+            var ministries = [];
+            angular.forEach(assignments, flattenMinistry);
+
+            function flattenMinistry(ministry) {
+                ministries.push(ministry);
+                if (ministry.hasOwnProperty('sub_ministries') && _.size(ministry.sub_ministries) > 0) {
+                    angular.forEach(ministry.sub_ministries, flattenMinistry);
+                }
+            }
+
+            return ministries;
+        }
 
     }
 
--- src/js/services/preference/user-preference-service.js
+++ src/js/services/preference/user-preference-service.js
@@ -9,31 +9,7 @@
             },
             savePreference: function (data) {
                 return $http.post(Settings.api.measurements('/user_preferences'), data)
-            },
-            getFlatMinistry: function (assignments) {
-                var ministries = [];
-                angular.forEach(assignments, flattenMinistry);
-
-                function flattenMinistry(ministry) {
-                    ministries.push(ministry);
-                    if (ministry.hasOwnProperty('sub_ministries') && _.size(ministry.sub_ministries) > 0) {
-                        angular.forEach(ministry.sub_ministries, flattenMinistry);
-                    }
-                }
-
-                return ministries;
-            },
-            getMappedMCCS: function (mccs, mccLabels) {
-                var mapped_mccs = [];
-                _.each(mccs, function (mcc) {
-                    var customObject = {};
-                    customObject.mcc = mcc;
-                    customObject.mccLabel = mccLabels[mcc];
-                    mapped_mccs.push(customObject);
-                });
-                return mapped_mccs;
             }
-
         };
 
     }
