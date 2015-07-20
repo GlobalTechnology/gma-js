863435fee37535aa921bf3e5560b3ee3243e5e26
austinankur <austin.ankur@ithands.net>
2015-07-20 11:25:06 +0530

    [#MMJS-66] Additional checks in mccFilter

--- src/js/filters/mcc-filter.js
+++ src/js/filters/mcc-filter.js
@@ -6,6 +6,9 @@
             return function (all_mccs, selected_mccs) {
 
                 var filtered = [];
+
+                if(typeof selected_mccs === 'undefined' || typeof all_mccs === 'undefined') return filtered;
+
                 angular.forEach(all_mccs, function (mcc) {
                     if (selected_mccs.indexOf(mcc.value) !== -1) {
                         filtered.push(mcc);
