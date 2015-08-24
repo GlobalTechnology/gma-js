ca296702535ab55fa03080ddd504b31e567805e4
austinankur <austin.ankur@ithands.net>
2015-08-24 14:31:15 +0530

    [#MMJS-5] Save content_locales and static_locale to user_preferences

--- src/js/controllers/preference/user-preference-ctrl.js
+++ src/js/controllers/preference/user-preference-ctrl.js
@@ -1,54 +1,77 @@
 (function () {
     'use strict';
-    function UserPreferenceCtrl($scope, $rootScope, $modalInstance, modelData, UserPreference,growl, Settings) {
-
+    function UserPreferenceCtrl($scope, $modalInstance, modelData, UserPreference, growl, Settings, MinistryLanguage) {
+        //set some defaults
         $scope.options = {
-            supported_staff : '0',
-            hide_reports_tab : '0'
+            supported_staff: '0',
+            hide_reports_tab: '0',
+            static_locale: 'en-us',
+            content_locales: {}
+        };
+        //set default lang for current ministry
+        $scope.options.content_locales[$scope.current.assignment.ministry_id] = 'en-us';
+        //collect data for looping
+        $scope.ministries = _.sortBy(UserPreference.getFlatMinistry($scope.current.assignments), 'name');
+        $scope.mccs = _.sortBy(UserPreference.getMappedMCCS($scope.current.assignment.mccs, modelData.mccLabels), 'mccLabel');
+        $scope.staticLocales = angular.copy(Settings.static_locales);
+
+        $scope.contentLocales = {};
+        MinistryLanguage.getLanguages()
+            .success(function (response) {
+                $scope.contentLocales = response;
+            })
+            .error(function () {
+                growl.error('Unable to load languages');
+            });
+
+        //override user preferences from token response
+        if (typeof $scope.current.user_preferences === 'object') {
+            $scope.options = angular.extend({}, $scope.options, angular.copy($scope.current.user_preferences));
+        }
+        //get supported languages by this ministry
+        var supportedLanguages = {};
+        if (typeof $scope.current.assignment.content_locales !== 'undefined') {
+            supportedLanguages = angular.copy($scope.current.assignment.content_locales);
+        }
+        $scope.filterByLangCode = function (lang) {
+
+            return (_.contains(supportedLanguages, lang.iso_code)) ? lang : false;
+
         };
 
-        $scope.ministries = _.sortBy(UserPreference.getFlatMinistry($rootScope.current.assignments),'name');
-
-        if(typeof $rootScope.current.user_preferences === 'object'){
-            $scope.options =  angular.extend({},$scope.options,angular.copy($rootScope.current.user_preferences));
-        }
-
-        $scope.mccs = _.sortBy(UserPreference.getMappedMCCS($rootScope.current.assignment.mccs, modelData.mccLabels),'mccLabel');
-
         $scope.savePreference = function (options) {
-
-                options.content_locales = {};
-                options.content_locales[$scope.current.assignment.ministry_id]=options.language ;
-                delete options.language;
+            //reduce payload weight
+            delete options.default_map_views;
+            delete options.default_measurement_states;
             UserPreference.savePreference(options)
                 .success(function (data) {
-                    growl.success('Preferences were saved');
+                    growl.success('Your preferences were saved');
                     //update root scope
-                    $rootScope.current.user_preferences = data;
+                    $scope.current.user_preferences = data;
                 })
-                .error(function(data, status, headers, config) {
+                .error(function () {
                     growl.error('Unable to save preferences');
                 });
-            $modalInstance.close();
+            $modalInstance.close(true);
         };
 
         $scope.changeMCCS = function (ministry_id) {
             if (ministry_id === "" || ministry_id === null) {
                 $scope.mccs = [];
-                $scope.options.preferred_mcc="";
+                $scope.options.preferred_mcc = "";
                 return false;
             }
             var ministry = _.find($scope.ministries, function (mini) {
                 return (mini.ministry_id === ministry_id);
             });
-            if(typeof ministry !== 'undefined') {
-                $scope.mccs = _.sortBy(UserPreference.getMappedMCCS(ministry.mccs, modelData.mccLabels),'mccLabel');
+            if (typeof ministry !== 'undefined') {
+                $scope.mccs = _.sortBy(UserPreference.getMappedMCCS(ministry.mccs, modelData.mccLabels), 'mccLabel');
                 //additional check if there is no mccs then default_mcc should be empty in every case
-                if(_.size($scope.mccs)==0){
-                    $scope.options.preferred_mcc="";
+                if (_.size($scope.mccs) == 0) {
+                    $scope.options.preferred_mcc = "";
                 }
-            }else{
-                $scope.options.preferred_mcc="";
+            } else {
+                $scope.options.preferred_mcc = "";
                 $scope.mccs = [];
             }
 
